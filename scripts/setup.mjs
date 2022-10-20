6ec8816b3073e9199a830547d5c461be667c135b
erhanb <67612501+sabancihan@users.noreply.github.com>
2022-10-20 15:29:22 +0300

    another bugfix

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -1,31 +1,28 @@
 import { MongoClient } from 'mongodb';
 import { faker } from '@faker-js/faker';
 import dotenv from 'dotenv';
-import User from 'models/User';
-import mongoose from 'mongoose';
-
 dotenv.config();
-
 const setup = async () => {
   let client;
-
   try {
-    client = await mongoose.connect(process.env.MONGODB_URI);
+    client = new MongoClient(process.env.MONGODB_URI);
+     await client.connect();
 
-    const hasData = await User.countDocuments().lean();
+     const hasData = await client
+       .db('football')
+       .collection('users')
+       .countDocuments();
 
     if (hasData) {
       console.log('Database already exists with data');
-      mongoose.disconnect();
+      client.close();
       return;
     }
-
     const records = [...Array(10)].map(() => {
       const [fName, lName] = faker.name.findName().split(' ');
       const username = faker.internet.userName(fName, lName);
       const email = faker.internet.email(fName, lName);
       const image = faker.image.people(640, 480, true);
-
       return {
         name: `${fName} ${lName}`,
         username,
@@ -35,26 +32,24 @@ const setup = async () => {
         emailVerified: null
       };
     });
-
-    const insert = await User
+    const insert = await client
+      .db('football')
+      .collection('users')
       .insertMany(records);
-
     if (insert.acknowledged) {
       console.log('Successfully inserted records');
     }
   } catch (error) {
     return 'Database is not ready yet';
   } finally {
-    if (mongoose) {
-      await mongoose.disconnect();
+    if (client) {
+      await client.close();
     }
   }
 };
-
 try {
   setup();
 } catch {
   console.warn('Database is not ready yet. Skipping seeding...');
 }
-
 export { setup };
