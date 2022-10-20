088e3369abcf49849705a4d2b47eb994794f327c
erhanb <67612501+sabancihan@users.noreply.github.com>
2022-10-20 21:55:25 +0300

    Update setup.mjs

--- scripts/setup.mjs
+++ scripts/setup.mjs
@@ -3,12 +3,6 @@ import { faker } from '@faker-js/faker';
 import dotenv from 'dotenv';
 import axios from 'axios';
 
-const baseUrl = "https://api.sofascore.com/api/v1"
-
-const topPlayers =  `${baseUrl}/unique-tournament/52/season/42632/statistics?limit=10&order=-goals&group=summary`
-
-
-
 
 dotenv.config();
 const setup = async () => {
@@ -27,27 +21,20 @@ const setup = async () => {
       client.close();
       return;
     }
-
-
-    const top10 = await axios.get(topPlayers);
-
-    const topPlayerInfo = await Promise.all(top10.data.map(
-      (stat) => axios.get(`${baseUrl}/${stat.player.id}`)
-    ));
-
-    const records = topPlayerInfo.map((record) => {
-      const player = record.data.player;
+    const records = [...Array(10)].map(() => {
+      const [fName, lName] = faker.name.findName().split(' ');
+      const username = faker.internet.userName(fName, lName);
+      const email = faker.internet.email(fName, lName);
+      const image = faker.image.people(640, 480, true);
       return {
-        name: player.name,
-        username : player.slug,
-        email : player.team.name,
-        image : `${baseUrl}/${player.id}/image`,
-        followers : faker.random.number(1000),
-        emailVerified : null
-      }
-    })
-
-
+        name: `${fName} ${lName}`,
+        username,
+        email,
+        image,
+        followers: 0,
+        emailVerified: null
+      };
+    });
     const insert = await client
       .db('football')
       .collection('users')
