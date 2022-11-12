723a7755c579ed0d54a0c580aa057d72821adc88
VainMayne <105226705+VainMayne@users.noreply.github.com>
2022-11-12 22:13:09 +0300

    navbar became global

---
+++ components/navbar.tsx
@@ -0,0 +1,45 @@
+import { useSession, signIn, signOut, getCsrfToken } from "next-auth/react"
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown'
+
+export default function Navbar(){
+    return(
+        
+        <nav className="navbar navbar-light navbar-expand-md py-3" style={{backgroundColor: '#6D6A75'}}>
+        <div className="container">
+            <a className="navbar-brand d-flex align-items-center" href="/main">
+            <span className="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-bezier">
+                <path fillRule="evenodd" d="M0 10.5A1.5 1.5 0 0 1 1.5 9h1A1.5 1.5 0 0 1 4 10.5v1A1.5 1.5 0 0 1 2.5 13h-1A1.5 1.5 0 0 1 0 11.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zm10.5.5A1.5 1.5 0 0 1 13.5 9h1a1.5 1.5 0 0 1 1.5 1.5v1a1.5 1.5 0 0 1-1.5 1.5h-1a1.5 1.5 0 0 1-1.5-1.5v-1zm1.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM6 4.5A1.5 1.5 0 0 1 7.5 3h1A1.5 1.5 0 0 1 10 4.5v1A1.5 1.5 0 0 1 8.5 7h-1A1.5 1.5 0 0 1 6 5.5v-1zM7.5 4a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
+                <path d="M6 4.5H1.866a1 1 0 1 0 0 1h2.668A6.517 6.517 0 0 0 1.814 9H2.5c.123 0 .244.015.358.043a5.517 5.517 0 0 1 3.185-3.185A1.503 1.503 0 0 1 6 5.5v-1zm3.957 1.358A1.5 1.5 0 0 0 10 5.5v-1h4.134a1 1 0 1 1 0 1h-2.668a6.517 6.517 0 0 1 2.72 3.5H13.5c-.123 0-.243.015-.358.043a5.517 5.517 0 0 0-3.185-3.185z" />
+                </svg>
+            </span>
+            <span>Scoutff</span>
+            </a>
+            <button data-bs-toggle="collapse" className="navbar-toggler" data-bs-target="#navcol-1"><span className="visually-hidden">Toggle navigation</span><span className="navbar-toggler-icon" /></button>
+            <div className="collapse navbar-collapse" id="navcol-1" style={{backgroundColor: '#6D6A75'}}>
+            <input type="search" style={{/*position: 'absolute', *//*alignSelf: 'center', *//*alignItems: 'center', *//*textAlign: 'center', */marginLeft: '400px'}} />
+            <ul className="navbar-nav ms-auto">
+                <DropdownButton
+                    
+                    title="Info"
+                    id="dropdown-menu-align-right"
+                    variant=""
+                        >
+                    <Dropdown.Item eventKey="option-1" href="/leaderboards">Leaderboards</Dropdown.Item>
+                    <Dropdown.Item eventKey="option-2">Squads</Dropdown.Item>
+                    <Dropdown.Item eventKey="option-3">Messages</Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item eventKey="some link">Privacy &amp; Security</Dropdown.Item>
+                </DropdownButton>
+                
+                <li className="nav-item"><a className="nav-link" href="/favourite">⭐ Favourite Players&nbsp;</a></li>                 
+                <li className="nav-item"><a className="nav-link" href="/profile">My Profile</a></li>
+                <li onClick={() => signOut({callbackUrl:"/auth/signin"})} className="nav-item"><a className="nav-link" href="#">Logout</a></li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+        
+    )
+}
---
+++ components/saha.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+export default function Saha(){
+    return(
+        <div>asdsa</div>
+        
+
+
+
+    )
+}
