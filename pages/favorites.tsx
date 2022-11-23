7b60d388735c7c788459badea43eb00331712baa
erhanb <67612501+sabancihan@users.noreply.github.com>
2022-11-23 09:22:31 +0300

    bisi

--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,88 +1,125 @@
-import { useSession, signIn, signOut, getCsrfToken, getSession } from "next-auth/react"
-import { Button } from "@chakra-ui/react";
-import Head from "next/head";
-import Image from "next/image";
+import {
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  useBreakpointValue,
+  useColorModeValue,
+  Wrap, 
+  Center,Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  Container,useToast,
+  TableCaption,
+  TableContainer, Square, Circle, Box, HStack, Grid, Spacer, Divider, VStack, ChakraProvider, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, FormControl, FormLabel, FormHelperText, Input, useDisclosure, Alert, AlertDescription, AlertIcon, AlertTitle,
+} from "@chakra-ui/react";
 import React from "react";
-import styles from "../styles/Home.module.css";
-import Link from "next/link";
-import HomeCompIndex from "../components/home/ui";
-import Navbar from "../components/layout/navbar/navbar";
-import FavoritesCompIndex from "../components/favorites/ui";
-import axios from "axios";
-import { GetServerSidePropsContext, InferGetServerSidePropsType, PreviewData } from "next";
-import { ParsedUrlQuery } from "querystring";
-import { getUserFavourites } from "../lib/api/user";
-import { getToken } from "next-auth/jwt";
-import invariant from "tiny-invariant";
-
-export default function Home({csrfToken,favourites} : InferGetServerSidePropsType<typeof getServerSideProps>) {
+import { useState } from 'react';
+import router, { useRouter } from "next/router";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { signIn } from "next-auth/react";
 
 
 
-  const addFavorite = async () => {
+export default function Forgot() {
 
-    const deneem = await axios.post("/api/user/favourites/63550795730a7fc502be606f", {
-      csrfToken: csrfToken,
-    })
+type FormValues = {
+  email: string;
+};
+
+const {
+  handleSubmit,
+  register,
+  formState: { errors, isSubmitting },
+} = useForm<FormValues>()
+
+const toast = useToast()
+const [errorMessage, setErrorMessage] = useState<String>("");
+
+const handleFormForgot :  SubmitHandler<FormValues>   = async (data) => {
 
 
 
 
-  
-
-
+  const emailSend = await signIn('email', {email : data.email, redirect: false})
+  if (emailSend && emailSend.error) {
+    setErrorMessage(emailSend.error)
+    return
   }
 
+  toast({
+    title: "Success.",
+    description: "We've sent you an email with a link to reset your password.",
+    status: "success",
+    duration: 9000,
+    isClosable: true,
+  })
+
+  setErrorMessage("")
+}
+  
   return (
-    <div>
-      <Head>
-        <title>Favorite Players | Scoutff</title>
-        <meta name="description" content="Scoutff 2022." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <Flex width="full" align="center" justifyContent="center">
+      <Box
+        p={8}
+        maxWidth="500px"
+        borderWidth={1}
+        borderRadius={8}
+        boxShadow="lg"
+      >
+        
+            <Box textAlign="center">
+              <Heading>Input your email address:</Heading>
+            </Box>
+            <Box my={4} textAlign="left">
+            <form onSubmit={handleSubmit(handleFormForgot)}>
+                <FormControl id="email" isRequired>
+                  <FormLabel>Email</FormLabel>
+                  <Input
+                    type="email"
+                    placeholder="test@test.com"
+                    size="lg"
+   
+                    {...register("email")}
+                    
+                  />
+                </FormControl>
 
-      <main>
-        {favourites && favourites.length  ? 
+                
+                
+                <Button
+                  variant="solid"
+                  type="submit"
+                  width="full"
+                  color={'blue'}
+                  mt={4}
+                >
+                  Submit
+                </Button>
+           </form>
 
-        <ul>
-          {favourites.map((fav,index) => (
-            <li key={fav + index.toString()}>{fav}</li>
-            
-          ))}
-        </ul>
-    
-
-          : 
-        <div>There are no favourites</div>}
-
-        <Button onClick={addFavorite} width="500px">
-          Add Arda Güler to your favorites
-        </Button>
-
-      </main>
-    </div>
+           {
+            errorMessage &&      
+            <Stack pt={3}>
+              <Alert status='error'>
+                <AlertIcon />
+                <AlertTitle>Error!</AlertTitle>
+                <AlertDescription>{errorMessage}</AlertDescription>
+              </Alert>
+           </Stack>
+          }
+            </Box>
+          
+        
+      </Box>
+    </Flex>
   );
 }
 
-
-export const getServerSideProps  = async (context : GetServerSidePropsContext<ParsedUrlQuery, PreviewData>) => {
-  const csrfToken = await getCsrfToken(context)
-
-  const session = await getToken(context)
-
-  const userId = session?.sub
-
-  const favourites = userId ? await getUserFavourites({userId}) : null
-
-
-
-
-
-  return {
-    props: { 
-      favourites,
-      csrfToken
-     },
-  }
-}
 
